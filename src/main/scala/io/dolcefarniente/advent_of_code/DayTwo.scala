5e73aed124fbb070e384172a7c0a61149a396af6
tlmnb <>
2020-12-02 12:36:19 +0100

    Added Solution for Day 2

---
+++ src/main/scala/io/dolcefarniente/advent_of_code/DayTwo.scala
@@ -0,0 +1,49 @@
+package io.dolcefarniente.advent_of_code
+
+import scala.io.Source
+
+trait PasswordPolicyValidator {
+
+  private val pwdRegex = "(\\d+)-(\\d+) (.): (.+)".r
+
+  def validate(pwd: String): Boolean = {
+    pwd match {
+      case pwdRegex(min, max, letter, p) => policy(min.toInt, max.toInt, letter(0), p)
+      case _ => false
+    }
+  }
+
+  def policy(min: Int, max: Int, letter: Char, password: String): Boolean
+}
+
+
+object PolicyOneValidator extends PasswordPolicyValidator {
+
+  override def policy(min: Int, max: Int, letter: Char, password: String): Boolean = {
+    (min to max).contains(password.count(_ == letter))
+  }
+
+}
+
+
+object PolicyTwoValidator extends PasswordPolicyValidator {
+
+  override def policy(min: Int, max: Int, letter: Char, password: String): Boolean = {
+    password(min-1) == letter ^ password(max-1) == letter
+  }
+
+}
+
+
+object DayTwo {
+
+  def main(args: Array[String]): Unit = {
+    val src = Source.fromResource("day/2/input")
+    val passwords = try src.getLines().toList finally src.close()
+    val policyOneCorrect = passwords.count(PolicyOneValidator.validate)
+    println(f"for policy one, $policyOneCorrect correct passwords were found")
+    val policyTwoCorrect = passwords.count(PolicyTwoValidator.validate)
+    println(f"for policy one, $policyTwoCorrect correct passwords were found")
+  }
+
+}
---
+++ src/test/scala/io/dolcefarniente/advent_of_code/DayTwoTest.scala
@@ -0,0 +1,20 @@
+package io.dolcefarniente.advent_of_code
+
+import org.scalatest.FunSuite
+
+class DayTwoTest extends FunSuite {
+
+  test("PolicyOne") {
+    assert(PolicyOneValidator.validate("1-3 a: abcde"))
+    assert(PolicyOneValidator.validate("2-9 c: ccccccccc"))
+    assert(!PolicyOneValidator.validate("1-3 b: cdefg"))
+  }
+
+  test("PolicyTwo") {
+    assert(PolicyOneValidator.validate("1-3 a: abcde"))
+    assert(PolicyOneValidator.validate("2-9 c: ccccccccc"))
+    assert(!PolicyOneValidator.validate("1-3 b: cdefg"))
+
+  }
+
+}
